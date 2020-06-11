2fce3dfd22b1feb0fa40c24562ffe61d532c0260
Andrew Alfred <andrewalfred99@yahoo.com>
2020-06-11 13:20:05 +0700

    Creating Projects inside seprate the project into multiple project, A Data Access and Models and Utility

---
+++ PharohBook.DataAccess/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace PharohBooks.DataAccess
+{
+    public class Class1
+    {
+    }
+}
---
+++ PharohBooks.Models/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace PharohBooks.Models
+{
+    public class Class1
+    {
+    }
+}
