03a3490c0ebb39936469e513c25605a797538a82
Andrew Alfred <andrewalfred99@yahoo.com>
2020-06-18 21:32:56 +0700

    Creating Category Repository and Interface

---
+++ PharohBook.DataAccess/Repository/CategoryRepository.cs
@@ -0,0 +1,34 @@
+﻿using PharohBooks.DataAccess.Data;
+using PharohBooks.DataAccess.Repository.IRepository;
+using PharohBooks.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace PharohBooks.DataAccess.Repository
+{
+  public class CategoryRepository : Repository<Category>, ICategoryRepository
+    {
+        private readonly ApplicationDbContext _db;
+
+
+        public CategoryRepository(ApplicationDbContext db): base(db)
+        {
+            _db = db;
+        }
+
+        public void update(Category category)
+        {
+            var objFromDb = _db.Categories.FirstOrDefault(s => s.Id == category.Id);
+            if (objFromDb != null)
+            {
+
+
+                objFromDb.Name = category.Name;
+
+                _db.SaveChanges();
+            }
+        }
+    }
+}
---
+++ PharohBook.DataAccess/Repository/IRepository/ICategoryRepository.cs
@@ -0,0 +1,12 @@
+﻿using PharohBooks.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PharohBooks.DataAccess.Repository.IRepository
+{
+   public interface ICategoryRepository : IRepository<Category>
+    {
+        void update(Category category);
+    }
+}
