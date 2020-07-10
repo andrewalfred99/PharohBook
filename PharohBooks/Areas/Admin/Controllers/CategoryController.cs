76a7aaa5bd22cbe2aa41df0546b08f7b670a028d
Andrew Alfred <andrewalfred99@yahoo.com>
2020-07-10 16:28:24 +0700

    Category Index and GetAll Actions

--- PharohBooks/Areas/Admin/Controllers/CategoryController.cs
+++ PharohBooks/Areas/Admin/Controllers/CategoryController.cs
@@ -3,14 +3,35 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using PharohBooks.DataAccess.Repository.IRepository;
 
 namespace PharohBooks.Areas.Admin.Controllers
+{ 
+  [Area("Admin")]
+public class CategoryController : Controller
 {
-    public class CategoryController : Controller
+    private readonly IUnitOfWork _unitOfWork;
+
+    public CategoryController(IUnitOfWork unitOfWork)
     {
-        public IActionResult Index()
-        {
-            return View();
-        }
+        _unitOfWork = unitOfWork;
     }
+
+    public IActionResult Index()
+    {
+        return View();
+    }
+
+
+    #region API CALLS
+
+    [HttpGet]
+    public IActionResult GetAll()
+    {
+        var allObj = _unitOfWork.Category.GetAll();
+        return Json(new { data = allObj });
+    }
+
+    #endregion
+}
 }
