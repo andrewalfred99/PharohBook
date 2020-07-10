4a174a76fd1a24f54eea37d32da1fa37afe9b7d3
Andrew Alfred <andrewalfred99@yahoo.com>
2020-07-10 17:40:18 +0700

    creayting view nad commen createAnd back and edit and back and  category jason file

--- PharohBooks/Areas/Admin/Controllers/CategoryController.cs
+++ PharohBooks/Areas/Admin/Controllers/CategoryController.cs
@@ -4,34 +4,35 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using PharohBooks.DataAccess.Repository.IRepository;
+using PharohBooks.Models;
 
 namespace PharohBooks.Areas.Admin.Controllers
-{ 
-  [Area("Admin")]
-public class CategoryController : Controller
 {
-    private readonly IUnitOfWork _unitOfWork;
-
-    public CategoryController(IUnitOfWork unitOfWork)
+    [Area("Admin")]
+    public class CategoryController : Controller
     {
-        _unitOfWork = unitOfWork;
+        private readonly IUnitOfWork _unitOfWork;
+
+        public CategoryController(IUnitOfWork unitOfWork)
+        {
+            _unitOfWork = unitOfWork;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+
+        #region API CALLS
+
+        [HttpGet]
+        public IActionResult GetAll()
+        {
+            var allObj = _unitOfWork.Category.GetAll();
+            return Json(new { data = allObj });
+        }
+
+        #endregion
     }
-
-    public IActionResult Index()
-    {
-        return View();
-    }
-
-
-    #region API CALLS
-
-    [HttpGet]
-    public IActionResult GetAll()
-    {
-        var allObj = _unitOfWork.Category.GetAll();
-        return Json(new { data = allObj });
-    }
-
-    #endregion
-}
 }
