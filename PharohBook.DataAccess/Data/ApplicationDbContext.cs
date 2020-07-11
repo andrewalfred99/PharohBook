a18f36eb86a598213f489b0fa293cec523e97c6c
Andrew Alfred <andrewalfred99@yahoo.com>
2020-07-11 11:27:40 +0700

    Add Cover Type Model and Repository Setup

--- PharohBook.DataAccess/Data/ApplicationDbContext.cs
+++ PharohBook.DataAccess/Data/ApplicationDbContext.cs
@@ -14,5 +14,6 @@ namespace PharohBooks.DataAccess.Data
         {
         }
         public DbSet<Category> Categories { get; set; }
+        public DbSet<CoverType> CoverTypes { get; set; }
     }
 }
--- PharohBooks.Models/CoverType.cs
+++ PharohBooks.Models/CoverType.cs
@@ -5,11 +5,11 @@ using System.Text;
 
 namespace PharohBooks.Models
 {
-   public class Category
+   public class CoverType
     {
         [Key]
         public int Id { get; set; }
-        [Display(Name="Category Name")]
+        [Display(Name="Cover Type")]
         [Required]
         [MaxLength(50)]
         public string Name { get; set; }
